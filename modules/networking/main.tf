39301ef21adb821dc2bd770700767caf9c500a24
hotpoopy <bryanttanyq98@gmail.com>
2024-02-16 06:08:51 +0800

    test

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -1,29 +1,24 @@
 #networking module main workflow
 
-#obtaining cidr_info from supposedly an api call form python script
-data "external" "cidr_info" {
-  program = ["python", "-c", file("${path.module}/../scripts/cidr_info.py")]
-}
-
-#create vpc
+# Create VPC using cidr_info
 resource "aws_vpc" "main" {
-  cidr_block = "${data.external.cidr_info.result.ip_address}${data.external.cidr_info.result.subnet_size}"
+  cidr_block = "${var.cidr_info.ip_address}${var.cidr_info.subnet_size}"
 }
 
-#create subnets
+# Create subnets
 resource "aws_subnet" "public" {
   count            = var.subnet_count
   vpc_id           = aws_vpc.main.id
-  cidr_block       = cidrsubnet("${data.external.cidr_info.result.ip_address}${data.external.cidr_info.result.subnet_size}", 8, count.index)
+  cidr_block       = cidrsubnet("${var.cidr_info.ip_address}${var.cidr_info.subnet_size}", 8, count.index)
   availability_zone = var.availability_zone
 }
 
-#networking setup
+# Networking setup
 resource "aws_internet_gateway" "my_igw" {
   vpc_id = aws_vpc.main.id
 }
 
-#route table
+# Route table
 resource "aws_route_table" "my_route_table" {
   vpc_id = aws_vpc.main.id
   
@@ -34,46 +29,47 @@ resource "aws_route_table" "my_route_table" {
 }
 
 resource "aws_route_table_association" "subnet_association" {
-  count = var.subnet_count
-  subnet_id = aws_subnet.public[count.index].id
+  count          = var.subnet_count
+  subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.my_route_table.id
 }
 
-#security group
+# Security group
 resource "aws_security_group" "EC2_security_group" {
-    name = "EC2_security_group"
-    description = "EC2 Security Group"
-    vpc_id = aws_vpc.main.id
+  name        = "EC2_security_group"
+  description = "EC2 Security Group"
+  vpc_id      = aws_vpc.main.id
 
-    #(http port 80)
-    ingress {
-      from_port = 80
-      to_port = 80
-      protocol = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-    }
-    #(https port 443)
-    ingress {
-      from_port   = 443
-      to_port     = 443
-      protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-    }
+  #(http port 80)
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-    #(ssh port 22)
-    ingress {
-      from_port = 22
-      to_port = 22
-      protocol = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-    }
+  #(https port 443)
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-    egress {
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    } 
+  #(ssh port 22)
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  } 
 }
 
 
--- modules/networking/variables.tf
+++ modules/networking/variables.tf
@@ -1,5 +1,16 @@
 #variables declared for networking module
 
+variable "cidr_info" {
+  type = object({
+    ip_address   = string
+    subnet_size  = string
+  })
+  default = {
+    ip_address   = "192.168.0.0"
+    subnet_size  = "/16"
+  }
+}
+
 variable "subnet_count" {
     description = "Number of subnets to create. Originally 256, but AWS free tier only allow 200"
     type = number
