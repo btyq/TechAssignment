8ac61103401a05760f9318c2522b4aadced18511
hotpoopy <bryanttanyq98@gmail.com>
2024-02-16 05:15:59 +0800

    test

--- main.tf
+++ main.tf
@@ -19,7 +19,8 @@ terraform {
 }
 
 provider "aws" {
-  profile = var.profile
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
   region = var.region
 }
 
--- variables.tf
+++ variables.tf
@@ -1,7 +1,9 @@
-variable "profile" {
-  description = "AWS profile name"
-  type = string
-  default = "TechnicalAssignment"
+variable "AWS_ACCESS_KEY_ID" {
+  description = "AWS ACCESS KEY ID"
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  description = "AWS SECRET ACCESS KEY"
 }
 
 variable "region" {
