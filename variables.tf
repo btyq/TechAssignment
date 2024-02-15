0e07c2a17b418ad48c4e5335a264afb655c22e00
hotpoopy <bryanttanyq98@gmail.com>
2024-02-16 05:50:10 +0800

    test

--- variables.tf
+++ variables.tf
@@ -1,11 +1,3 @@
-variable "AWS_ACCESS_KEY_ID" {
-  description = "AWS_ACCESS_KEY_ID"
-}
-
-variable "AWS_SECRET_ACCESS_KEY" {
-  description = "AWS_SECRET_ACCESS_KEY"
-}
-
 variable "region" {
     description = "AWS region"
     default = "us-east-1" 
